c9f1c13ce969a885389d5a78600b54aba7101483
Newton Yuan <newton@tella.co.nz>
2024-01-30 15:39:42 +1300

    work: start button and moving timer circle

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:math';
+import 'dart:math' as math;
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -26,9 +27,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _counter = ValueNotifier<int>(0);
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -72,31 +79,87 @@ class Home extends StatelessWidget {
                 Align(
                   alignment: Alignment.center,
                   child: Container(
-                    margin: const EdgeInsets.symmetric(vertical: 70),
+                    margin: const EdgeInsets.symmetric(vertical: 48),
                     alignment: Alignment.center,
-                    height: 280,
-                    width: 280,
                     decoration: const BoxDecoration(
                       color: darkOutineColor,
                       shape: BoxShape.circle,
                     ),
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(
-                          color: primaryColor,
-                          shape: BoxShape.circle,
-                          border: Border.all(width: 3, color: outlineColor)),
-                      child: Text(
-                        "43:33",
-                        style: Theme.of(context).textTheme.displayLarge,
-                      ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ValueListenableBuilder<int>(
+                          valueListenable: _counter,
+                          builder: (context, value, child) {
+                            return TweenAnimationBuilder(
+                              duration: const Duration(seconds: 0),
+                              tween: Tween(begin: 0.0, end: 1.0),
+                              curve: Curves.easeOutCubic,
+                              builder: (BuildContext context, dynamic value,
+                                  Widget? child) {
+                                return Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    CustomPaint(
+                                      painter: OpenPainter(
+                                          timeElapsed: _counter.value,
+                                          totalTime: 10,
+                                          aniValue: value,
+                                          notifier: _counter),
+                                    ),
+                                    Container(
+                                        alignment: Alignment.center,
+                                        height: 240,
+                                        width: 240,
+                                        decoration: BoxDecoration(
+                                            color: primaryColor,
+                                            shape: BoxShape.circle,
+                                            border: Border.all(
+                                                width: 2, color: outlineColor)),
+                                        child: Text(
+                                          "43:33",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .displayLarge,
+                                        ))
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+                        const SizedBox(
+                            height:
+                                48), // Adjust the space between the two containers
+                        GestureDetector(
+                          onTapUp: (TapUpDetails details) {
+                            if (_counter.value < 10) {
+                              _counter.value++;
+                            } else {
+                              _counter.value = 0;
+                            }
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 50,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                color: primaryColor,
+                                shape: BoxShape.rectangle,
+                                border:
+                                    Border.all(width: 2, color: outlineColor),
+                                borderRadius: BorderRadius.circular(16)),
+                            child: const Text("Start",
+                                style: TextStyle(
+                                    fontSize: 24, color: outlineColor)),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 ),
                 Container(
-                  //margin: const EdgeInsets.only(bottom: 80),
+                  margin: const EdgeInsets.only(top: 48),
                   height: 200,
                   decoration: const BoxDecoration(
                     image: DecorationImage(
@@ -112,4 +175,35 @@ class Home extends StatelessWidget {
       ),
     );
   }
+}
+
+class OpenPainter extends CustomPainter {
+  int timeElapsed;
+  int totalTime;
+  final aniValue;
+  double pi = math.pi;
+  final ValueNotifier<int> notifier;
+
+  OpenPainter(
+      {required this.timeElapsed,
+      required this.totalTime,
+      this.aniValue,
+      required this.notifier})
+      : super(repaint: notifier);
+  @override
+  void paint(Canvas canvas, Size size) {
+    Rect rect = const Offset(-120.0, 0) & const Size(240.0, 240.0);
+
+    var paint = Paint()
+      ..color = outlineColor
+      ..strokeWidth = 24
+      ..style = PaintingStyle.stroke;
+
+    double start = pi * 1.5;
+    double end = (360 * (timeElapsed / totalTime)) * math.pi / 180 * aniValue;
+    canvas.drawArc(rect, start, end, false, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
